6d1f9bd1e31672c32aa63c236963291e6710261b
EdgeSan <edgar.kalinovski@teliacompany.com>
2022-04-01 15:36:22 +0300

    Add scritp.sh file to the repo

---
+++ script.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+set -eou pipefail 
+
+REPONAME="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 2)" 
+OWNER="$(echo $GITHUB_REPOSITORY| cut -d'/' -f 1)" 
+GHIO="${OWNER}.github.io"
+if [[ "$REPONAME" == "$GHIO" ]]; then
+  TARGET_BRANCH="main"
+else
+  TARGET_BRANCH="gh-pages"
+fi
+
+if [ ! -z "$INPUT_BRANCH" ]; then 
+  TARGET_BRANCH=$INPUT_BRANCH
+fi
+
+echo "Starting deploy to $GITHUB_REPOSITORY/$TARGET_BRANCH"
+
+echo "Configuration:"
+echo "- email: $INPUT_EMAIL"
+echo "- build_dir: $INPUT_BUILD_DIR"
+echo "- cname: $INPUT_CNAME"
+echo "- Jekyll: $INPUT_JEKYLL"
+
+BUILD_DIR=$INPUT_BUILD_DIR
+BUILD_DIR=${BUILD_DIR%/} 
+mkdir -p $HOME/build/$BUILD_DIR
+cp -R $BUILD_DIR/* $HOME/build/$BUILD_DIR/
+
+mkdir -p $HOME/branch/
+cd $HOME/branch/
+git config --global user.name "$GITHUB_ACTOR"
+git config --global user.email "$INPUT_EMAIL"
+if [ -z "$(git ls-remote --heads https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git ${TARGET_BRANCH})" ]; then
+  echo "Create branch '${TARGET_BRANCH}'"
+  git clone --quiet https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git $TARGET_BRANCH > /dev/null 
+  cd $TARGET_BRANCH
+  git checkout --orphan $TARGET_BRANCH
+  git rm -rf .
+  echo "$REPONAME" > README.md
+  git add README.md
+  git commit -a -m "Create '$TARGET_BRANCH' branch"
+  git push origin $TARGET_BRANCH
+  cd ..
+else
+  echo "Clone branch '${TARGET_BRANCH}'"
+  git clone --quiet --branch=$TARGET_BRANCH https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git $TARGET_BRANCH > /dev/null
+fi
+
+cp -R $TARGET_BRANCH/.git $HOME/build/$BUILD_DIR/.git
+rm -rf $TARGET_BRANCH/*
+cp -R $HOME/build/$BUILD_DIR/.git $TARGET_BRANCH/.git
+
+cd $TARGET_BRANCH
+cp -Rf $HOME/build/$BUILD_DIR/* .
+
+if [ ! -z "$INPUT_CNAME" ]; then
+  echo "$INPUT_CNAME" > CNAME
+fi
+
+if [ -z "$INPUT_COMMIT_MESSAGE" ]
+then
+  INPUT_COMMIT_MESSAGE="$GITHUB_ACTOR published a site update"
+fi
+
+if [ "$INPUT_JEKYLL" != "yes" ]; then
+  touch .nojekyll
+fi
+
+if [ -z "$(git status --porcelain)" ]; then 
+  result="Nothing to deploy"
+else
+  git add -Af .
+  git commit -m "$INPUT_COMMIT_MESSAGE"
+  git push -fq origin $TARGET_BRANCH > /dev/null
+
+  if [ $? = 0 ]
+  then
+    result="Deployed successfully"
+  else
+    result="Failed"
+  fi
+fi
